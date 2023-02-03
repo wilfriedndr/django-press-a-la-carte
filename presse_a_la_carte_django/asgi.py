4828d90eb80cba896c43bc7449b89c45e57cd3f7
Wilfried <wilfried.ndr@gmail.com>
2023-02-03 10:21:49 +0100

    init django project

---
+++ presse_a_la_carte_django/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for presse_a_la_carte_django project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'presse_a_la_carte_django.settings')
+
+application = get_asgi_application()
