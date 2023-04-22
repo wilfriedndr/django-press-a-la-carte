bcd7e709a1eda2bd907d8413572f96bc2ae7affc
dorinebertonTlse <dorineberton@gmail.com>
2023-04-22 14:56:54 +0200

    install tailwind

--- presse_a_la_carte_django/settings.py
+++ presse_a_la_carte_django/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+
+# rafraichissement auto
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
