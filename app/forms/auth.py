6873b86ee4db3e219951273a1e88629643690d05
Wilfried <wilfried.ndr@gmail.com>
2023-05-20 17:25:55 +0200

    init branch

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -1,12 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=65)
-    password = forms.CharField(max_length=65, widget=forms.PasswordInput)
-    
+def my_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        return "login successful"
+    else:
+        # Return an 'invalid login' error message.
+        return "login unsuccessful"
 
 class RegisterForm(UserCreationForm):
     class Meta:
