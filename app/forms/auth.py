f395828e8f23b70b782cbe89e779ed9dbf5c2745
Wilfried <wilfried.ndr@gmail.com>
2023-05-20 17:32:05 +0200

    auth/registration base

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 
 
-def my_view(request):
+def LoginForm(request):
     username = request.POST["username"]
     password = request.POST["password"]
     user = authenticate(request, username=username, password=password)
@@ -16,7 +16,15 @@ def my_view(request):
         # Return an 'invalid login' error message.
         return "login unsuccessful"
 
-class RegisterForm(UserCreationForm):
-    class Meta:
-        model=User
-        fields = ['username','email','password'] 
+def RegisterForm(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    email = request.POST["email"]
+    user = authenticate(request, username=username, password=password, email=email)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        return "regitration successful"
+    else:
+        # Return an 'invalid login' error message.
+        return "registration unsuccessful"
